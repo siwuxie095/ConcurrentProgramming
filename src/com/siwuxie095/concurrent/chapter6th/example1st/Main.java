11b854025ed018d1f9c1ced41147ff4ccf77e087
siwuxie095 <834879583@qq.com>
2020-09-28 07:17:20 +0800

    Java 8 与并发；

---
+++ src/com/siwuxie095/concurrent/chapter6th/example1st/Main.java
@@ -0,0 +1,23 @@
+package com.siwuxie095.concurrent.chapter6th.example1st;
+
+/**
+ * @author Jiajing Li
+ * @date 2020-09-28 07:04:50
+ */
+public class Main {
+
+    /**
+     * Java 8 与并发
+     *
+     * 2014 年，Oracle 发布了 Java 8 新版本。对于 Java 来说，这是一个具有里程碑意义的版本。它最主要的改进是
+     * 增加了函数式编程的功能。就目前来说，Java 最令人头痛的问题，也是受到最多质疑的地方，应该就是 Java 那烦琐
+     * 的语法。这样就不得不花费大量的代码行数，来实现一些司空见惯的功能，以至于 Java 程序总是冗长的。但是，这一
+     * 切将在 Java 8 的函数式编程中得到缓解。
+     *
+     * 看起来函数式编程和并发关系不大，实际上 Java 8 中新增的一些与并行相关的 API，是以函数式编程的范式出现的。
+     */
+    public static void main(String[] args) {
+
+    }
+
+}
